c6daadbb86ed02b1e3e2da99810245a82fba8748
Christian <111076892+i-christian@users.noreply.github.com>
2024-02-15 16:42:04 +0200

    finalise the app

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -12,13 +12,13 @@ const Features = () => {
           <div>
             <img src={Feature_One} alt="Feature 1" />
             <h3>2D view of the game map</h3>
-            <p>The game has two views: the 2D view and the projected 3D view. These views can be toggled between using the M key on the keyboard.</p>
+             <p>Our project aims to provide players with an immersive gaming experience by offering two distinct views: the traditional 2D view and an engaging projected 3D view. With seamless toggling between these perspectives using the M key on the keyboard, players can explore the game world from different angles, enhancing their gameplay experience.</p>
           </div>
           {/* Feature 2 */}
           <div>
             <img src={Feature_Two} alt="Feature 2" />
             <h3>Collision detection of the game</h3>
-            <p>This feature allows player movement to be restricted only to empty spaces. Thus a player should not be able to pass through walls.</p>
+             <p>Ensuring realism and fair play, our collision detection feature restricts player movement solely to empty spaces within the game environment. By preventing players from passing through walls or other obstacles, we maintain the integrity of the game's challenges and enhance the overall gaming experience.</p>
           </div>
           {/* Feature 3 */}
           <div>
@@ -26,7 +26,7 @@ const Features = () => {
              <img style={{ maxWidth: '300px' }} src="https://cdn.loom.com/sessions/thumbnails/bfd051a15a3c4707ad57214fcfdc4052-with-play.gif" alt="Video Thumbnail" />
             </a>
             <h3>Player movement</h3>
-            <p>Using the arrow keys to show player movement and rotation.</p>
+            <p>With intuitive controls using arrow keys, our project showcases fluid player movement and rotation. This enhances user engagement and immersion, allowing players to navigate through the game world with ease and precision, ultimately contributing to a more enjoyable gaming experience.</p>
           </div>
         </div>
       </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -11,7 +12,6 @@ const Footer = () => {
     });
   };
 
-  // Show/hide the button based on scroll position
   window.onscroll = function() { scrollFunction() };
 
   const scrollFunction = () => {
@@ -27,15 +27,17 @@ const Footer = () => {
     <footer id="about" className="fade-in-up">
       <div className="max-container">
         <div className="footer-column">
-          <h2>About Our Project</h2>
-          <p>Our project aims to revolutionize...</p>
-          <p>Developed as part of the Portfolio Project at Holberton School.</p>
+          <h2>About</h2>
+          <p>PixelEscape is a groundbreaking gaming project that seeks to redefine the gaming experience. Our mission is to provide players with a captivating and immersive journey through innovative features and engaging gameplay.</p>
+          <p>Developed as part of the Portfolio Project at Holberton School, PixelEscape represents our commitment to pushing the boundaries of creativity and technical excellence in the world of game development.</p>
         </div>
         <div className="footer-column">
           <h2>Follow Us</h2>
           <div className="social-links">
-            <a href="#" className="fade-in-up">LinkedIn</a>
-            <a href="#" className="fade-in-up">GitHub</a>
+            <a href="https://www.linkedin.com/in/i-christian/" className="fade-in-up"><FaLinkedin /></a>
+            <a href="https://github.com/i-christian/" className="fade-in-up"><FaGithub /></a>
+            <a href="https://www.linkedin.com/in/mkajiwonele/" className="fade-in-up"><FaLinkedin /></a>
+            <a href="https://github.com/honorifia/" className="fade-in-up"><FaGithub /></a>
           </div>
         </div>
       </div>
