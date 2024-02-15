abbba9abfc11574fb6bb70fad0c5d40173eac9d0
Christian <111076892+i-christian@users.noreply.github.com>
2024-02-15 16:05:46 +0200

    finishing up page

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Feature_One from '../assets/img/2D.png';
+import Feature_Two from '../assets/img/Collision.png';
 
 const Features = () => {
   return (
@@ -8,21 +10,23 @@ const Features = () => {
         <div className="feature-grid">
           {/* Feature 1 */}
           <div>
-            <img src="path_to_feature_image_1" alt="Feature 1" />
-            <h3>Feature 1 Name</h3>
-            <p>Description of Feature 1</p>
+            <img src={Feature_One} alt="Feature 1" />
+            <h3>2D view of the game map</h3>
+            <p>The game has two views: the 2D view and the projected 3D view. These views can be toggled between using the M key on the keyboard.</p>
           </div>
           {/* Feature 2 */}
           <div>
-            <img src="path_to_feature_image_2" alt="Feature 2" />
-            <h3>Feature 2 Name</h3>
-            <p>Description of Feature 2</p>
+            <img src={Feature_Two} alt="Feature 2" />
+            <h3>Collision detection of the game</h3>
+            <p>This feature allows player movement to be restricted only to empty spaces. Thus a player should not be able to pass through walls.</p>
           </div>
           {/* Feature 3 */}
           <div>
-            <img src="path_to_feature_image_3" alt="Feature 3" />
-            <h3>Feature 3 Name</h3>
-            <p>Description of Feature 3</p>
+            <a href="https://www.loom.com/share/bfd051a15a3c4707ad57214fcfdc4052">
+             <img style={{ maxWidth: '300px' }} src="https://cdn.loom.com/sessions/thumbnails/bfd051a15a3c4707ad57214fcfdc4052-with-play.gif" alt="Video Thumbnail" />
+            </a>
+            <h3>Player movement</h3>
+            <p>Using the arrow keys to show player movement and rotation.</p>
           </div>
         </div>
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Cover from '../assets/img/Cover.png';
 
 const Header = () => {
   return (
     <header id="intro">
       <section className="max-container">
-        <img src="path_to_cover_image" alt="Project Cover" className="fade-in-up" />
+        <img src={Cover} alt="Project Cover" className="fade-in-up" />
         <h1 className="fade-in-up">PixelEscape</h1>
         <p className="fade-in-up">Escape the Pixelated Labyrinth: A 3D Raycasting Maze Adventure</p>
 
@@ -15,7 +16,7 @@ const Header = () => {
           </ul>
         </nav>
 
-        <a href="link_to_project_video" className="explore-btn fade-in-up">Explore Now</a>
+        <a href="https://www.loom.com/share/8d298c9eba4a4f489d6a9c8a065c3680?sid=713813bc-1212-487b-9e69-58faa267e5f6" className="explore-btn fade-in-up">Explore Now</a>
       </section>
     </header>
   );
